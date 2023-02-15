35e87caf9f794a610023264b32eba367bfb59ee1
jinho-choi123 <choijinho817@gmail.com>
2023-02-15 16:44:31 +0000

    add owner id argument for deleting wish

--- src/db/api/wish/delete.ts
+++ src/db/api/wish/delete.ts
@@ -1,11 +1,12 @@
 import {ObjectId} from 'mongoose';
 import Wish from '../../schema/wish';
 
-const deleteWish = (wishId: ObjectId) => {
+const deleteWish = (owner: ObjectId, wishId: ObjectId) => {
     return new Promise((res, rej) => {
         Wish.deleteOne(
             {
                 _id: wishId,
+                owner: owner,
             })
             .then((result) => {
                 res(result);
