7e83d14a9570fde20da41d4f424d1b833bf93488
jinho-choi123 <choijinho817@gmail.com>
2023-02-15 15:08:11 +0000

    implement find user by userId

--- src/db/api/user/find.ts
+++ src/db/api/user/find.ts
@@ -1,10 +1,18 @@
+import { ObjectId } from 'mongoose';
 import User from '../../schema/user';
 
-const findUser = (provider: String, snsId: String) => {
+const findUser = (userId: ObjectId) => {
     return new Promise((res, rej) => {
         User
-            .findOne({provider: provider, snsId: snsId})
-            .then((user) => res(user))
+            .findOne({_id: userId})
+            .then((user) => {
+                if (user == null) {
+                    rej("User not exists");
+                }
+                res(user)
+            })
             .catch((err) => rej(err))
     })
 }
+
+export default findUser;
