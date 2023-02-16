cc52ca91a8ce599f7768049f9c3b41658dda2cb4
jinho-choi123 <choijinho817@gmail.com>
2023-02-16 15:37:29 +0000

    add kakao router for login and logout. need fix

--- src/router/auth/authRouter.ts
+++ src/router/auth/authRouter.ts
@@ -1,16 +1,22 @@
-import express, {Router, Request, Response} from 'express';
+import express, {Router, Request, Response, NextFunction} from 'express';
 import passport from 'passport';
-import passportConfig from '../../utils/passport/index';
-passportConfig();
-
+import { nextTick } from 'process';
 const authRouter: Router = express.Router();
 
 authRouter.get('/kakao', passport.authenticate('kakao'));
 
 authRouter.get('/kakao/callback', passport.authenticate('kakao', 
     {
-        failureRedirect: '/checkserver',
-        successRedirect: '/checkserver',
-    }));
+        failureRedirect: '/',
+    }), (req: Request, res: Response) => {
+        res.redirect('/checkserver')
+    });
+
+authRouter.post('/kakao/logout', (req: Request, res: Response, next: NextFunction) => {
+    req.logout((err) => {
+        if(err) {return res.redirect('/failed')}
+        else {res.redirect('/');}
+    })
+})
 
 export default authRouter;
