d72ab038ffeadf316a7228c4087f3d17c6dcfbb1
jinho-choi123 <choijinho817@gmail.com>
2023-02-17 08:21:48 +0000

    init product db

---
+++ src/db/api/product/mockData.ts
@@ -0,0 +1,88 @@
+const mockData = [
+    {
+        "product_name": "mango",
+        "product_description": "Fresh mango from Channeltalk",
+        "currency": "KRW",
+        "price": 5000
+    },
+    {
+        "product_name": "macbook m2 air",
+        "product_description": "I like mango",
+        "currency": "KRW",
+        "price": 1000000
+    },
+    {
+        "product_name": "m2 macmini",
+        "product_description": "I like mango",
+        "currency": "KRW",
+        "price": 800000
+    },
+    {
+        "product_name": "15inch macbook m1 pro",
+        "product_description": "I love mango",
+        "currency": "KRW",
+        "price": 1100000
+    },
+    {
+        "product_name": "Samsung Galaxy book3 Ultra",
+        "product_description": "I really like mango",
+        "currency": "KRW",
+        "price": 1300000
+    },
+    {
+        "product_name": "Samsung monitor S24R35A",
+        "product_description": "mango?",
+        "currency": "KRW",
+        "price": 152000
+    },
+    {
+        "product_name": "Samsung Odyssey G5 gaming monitor",
+        "product_description": "mango!",
+        "currency": "KRW",
+        "price": 336000
+    },
+    {
+        "product_name": "JBL Flip6 bluetooth speaker",
+        "product_description": "Number1 outdoor speaker",
+        "currency": "KRW",
+        "price": 159000
+    },
+    {
+        "product_name": "Logitech K380 white keyboard",
+        "product_description": "convenient keyboard.",
+        "currency": "KRW",
+        "price": 35900
+    },
+    {
+        "product_name": "Apple magic mouse white",
+        "product_description": "apple < mango",
+        "currency": "KRW",
+        "price": 82900
+    },
+    {
+        "product_name": "Apple iphone 14 pro",
+        "product_description": "quite good phone",
+        "currency": "KRW",
+        "price": 1589000
+    },
+    {
+        "product_name": "Belkin 7in1 C-type hub",
+        "product_description": "mango",
+        "currency": "KRW",
+        "price": 69900
+    },
+    {
+        "product_name": "Apple mac-safe charger",
+        "product_description": "mango",
+        "currency": "KRW",
+        "price": 53010
+    },
+    {
+        "product_name": "mango cookie",
+        "product_description": "Fresh mango from Channeltalk",
+        "currency": "KRW",
+        "price": 1000
+    },
+]
+
+export default mockData;
