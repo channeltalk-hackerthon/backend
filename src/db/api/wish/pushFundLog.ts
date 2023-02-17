1d4e5407d57abe5953c212017da636b9c11ca482
jinho-choi123 <choijinho817@gmail.com>
2023-02-17 10:25:04 +0000

    push fundlogid from wish fundlogs

---
+++ src/db/api/wish/pushFundLog.ts
@@ -0,0 +1,22 @@
+import mongoose, {ObjectId} from 'mongoose';
+import Wish from '../../schema/wish';
+
+const pushFundLog = (userId: mongoose.Types.ObjectId, fundLogId: mongoose.Types.ObjectId) => {
+    return new Promise((res, rej) => {
+        Wish.updateOne({
+            _id: userId,
+        }, {
+            $push: {
+                fundlogs: fundLogId,
+            }
+        })
+            .then((doc) => {
+                res(doc);
+            })
+            .catch((err) => {
+                rej(err);
+            })
+    })
+}
+
+export default pushFundLog;
